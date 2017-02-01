fb41f0879678852068ad9a2889428c810430649f
Ivan Seregin <ivan.seregin@gmail.com>
2017-02-01 21:13:35 +0700

    Atm in progress

--- Khasang/ATM/src/Main.java
+++ Khasang/ATM/src/Main.java
@@ -1,8 +1,49 @@
+import java.io.*;
+
 /**
  * Created by igor on 14.08.16.
  */
 public class Main {
     public static void main(String[] args) {
+
+        class Cat implements Serializable{
+            String name;
+
+            public Cat(){};
+
+            public Cat(String name){
+                this.name = name;
+            }
+
+            @Override
+            public String toString() {
+                return name;
+            }
+        }
+
+        Cat cat = new Cat("Barsik");
+
+        try (ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream (new FileOutputStream("cat.txt", false)))){
+            out.writeObject(cat);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Cat cat1 = new Cat();
+        try (ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(new FileInputStream("cat.txt")))){
+
+            cat1 = (Cat) in.readObject();
+        } catch (IOException e){
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(cat1);
+    }
+
+        /*
         Atm atm = new Atm();
         Card masterCard = new MasterCard(100);
         Card visa = new Visa(300);
@@ -24,5 +65,5 @@ public class Main {
         Visa visa1 = new Visa(300);
         atm.eject();
         atm.insertCard(visa1);
-    }
+        */
 }
